ee1162f841be83f35b9066e085cb7d3996d2291e
w4sQ <maks.sinitsky@yandex.ru>
2023-02-07 18:45:20 +0300

    Add '/check', and delete some useless command

--- handlers/message.py
+++ handlers/message.py
@@ -1,11 +1,10 @@
-import asyncio
 import logging
-import random
 
 from aiogram import Dispatcher
 from aiogram.types import Message
 
-from helper import is_private, is_group # type: ignore
+from config import servers
+from helper import is_private, is_group, Server  # type: ignore
 from keyboards import main_menu
 
 logger = logging.getLogger(__name__)
@@ -15,26 +14,20 @@ async def start(message: Message):
     await message.reply("Hello, my friend!", reply_markup=main_menu())
 
 
-async def spam(message: Message):
-    if message.reply_to_message is not None:
-        spam_user = message.reply_to_message.from_user.username
-        await message.reply(spam_user)
-    else:
-        pass
-
-
-async def cum(message: Message):
-    text = "Размер вашей cum-пушки: {size} см."
-    msg = await message.reply(text.format(size=random.randint(3, 44)), protect_content=True)
-    sticker = await message.reply_sticker('CAACAgIAAxkBAAEHYVljzGF0ux80GskQWlNb6UsLzex-QAACxw8AAj-VwUqoJZJZftReKS0E')
-    await asyncio.sleep(2.5)
-    await msg.edit_text(text.format(size=random.randint(1, 4)))
-    await sticker.delete()
+async def check(message: Message):
+    server_name = message.get_args()
+    text = 'Такого сервера нет в списке, доступные сервера:\n' \
+           f'`{"`, `".join(servers.keys())}`'
+    if not server_name:
+        text = 'Укажите сервер:\n' \
+               '`/check <название>`'
+    if server_name in servers:
+        server = Server(servers[server_name])
+        text = server.check()
+    await message.reply(text, parse_mode='Markdown')
 
 
 def register(dp: Dispatcher):
     dp.register_message_handler(start, commands='start')
-    dp.register_message_handler(spam, is_group(), commands='spam')
-    dp.register_message_handler(cum, commands='cum')
-
+    dp.register_message_handler(check, commands='check')
     logger.debug('handlers.message registered')
--- helper.py
+++ helper.py
@@ -1,7 +1,12 @@
 import asyncio
+import logging
 
+import mcstatus.pinger
 from aiogram.dispatcher.filters import BoundFilter
 from aiogram.types import Message
+from mcstatus import JavaServer
+
+logger = logging.getLogger(__name__)
 
 
 class is_private(BoundFilter):
@@ -22,3 +27,36 @@ class is_group(BoundFilter):
         msg = await message.answer("Это команда достпуна только в групповых чатах!", disable_notification=True)
         await asyncio.sleep(1.5)
         await msg.delete()
+
+
+class Server:
+
+    def __init__(self, address: str):
+        self.address = address
+
+    def check(self) -> str:
+        players = self.players_online()
+        if players:
+            msg = f'Онлайн: {len(players)}\n' \
+                  f'Игроки: {", ".join(players)}'
+            return msg
+        return 'Сервер выключен!'
+
+    def players_online(self) -> list | None:
+        player_list = []
+        resp = self.__connection_to_server()
+        if isinstance(resp, mcstatus.pinger.PingResponse):
+            raw_player_list = resp.raw['players']['sample']
+            for player in raw_player_list:
+                player_list.append(player['name'])
+            return player_list
+
+    # 'players'
+    def __connection_to_server(self) -> mcstatus.pinger.PingResponse | None:
+        try:
+            server = JavaServer.lookup(address=self.address, timeout=10)
+            logger.info(f'Connected to {self.address}')
+            resp = server.status()
+            return resp
+        except TimeoutError:
+            logger.warning(f'{self.address} is offline')
