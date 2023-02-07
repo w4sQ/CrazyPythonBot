b26f63d134a7b26c6c80458b4ee10b6f3d8f132b
w4sQ <maks.sinitsky@yandex.ru>
2023-02-07 20:10:03 +0300

    Fuck chars

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
 from os import environ
 
 from aiogram import Bot, Dispatcher
-from aiogram import types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-bot = Bot(environ['BOT_TOKEN'], parse_mode=types.ParseMode.HTML)
+bot = Bot(environ['BOT_TOKEN'])
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
